808c0258fdd1659b2fbaf64938e94f3b4c04f29f
Orlando Hoilett <orlandohoilett@pal-nat186-5-208.itap.purdue.edu>
2018-06-19 05:56:44 -0400

    updates to CyclicVoltammetry example to include settling time before measurements

--- examples/CyclicVoltammetry/CyclicVoltammetry.ino
+++ examples/CyclicVoltammetry/CyclicVoltammetry.ino
@@ -3,58 +3,72 @@
 
 LMP91000 pstat = LMP91000();
 
+int rate = 200;
+int settling_time = 50;
+
 void setup()
 {
   Wire.begin();
-  Serial.begin(115200);
+  Serial.begin(9600);
 
   delay(50);
   pstat.standby();
   delay(50);
   pstat.disableFET();
+  pstat.setGain(1);
+  pstat.setRLoad(0);
+  pstat.setExtRefSource();
+  pstat.setIntZ(1);
   pstat.setThreeLead();
-  pstat.setGain(2);
-
+  delay(2000); //warm-up time for the gas sensor
 
   for (int j = 0; j < 3; j++)
   {
     pstat.setNegBias();
-    for (int i = 0; i < 11; i++)
+    for (int i = 1; i < 11; i++)
     {
       pstat.setBias(i);
+      //delay(50);
+      delay(settling_time);
       Serial.print(i*-1);
       Serial.print(",");
       delay(1);
       Serial.println(analogRead(A0));
-      delay(50);
+      delay(rate);
     }
     for (int i = 10; i >= 0; i--)
     {
       pstat.setBias(i);
+      //delay(50);
+      delay(settling_time);
       Serial.print(i*-1);
       Serial.print(",");
       delay(1);
       Serial.println(analogRead(A0));
-      delay(50);
+      delay(rate);
     }
     pstat.setPosBias();
     for (int i = 1; i < 11; i++)
     {
       pstat.setBias(i);
+      //delay(50);
+      delay(settling_time);
       Serial.print(i*1);
       Serial.print(",");
       delay(1);
       Serial.println(analogRead(A0));
-      delay(50);
+      delay(rate);
     }
     for (int i = 10; i >= 0; i--)
     {
       pstat.setBias(i);
+      //delay(50);
+      delay(settling_time);
       Serial.print(i*1);
       Serial.print(",");
       delay(1);
       Serial.println(analogRead(A0));
-      delay(50);
+      delay(rate);
     }
   }
   
