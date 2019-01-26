64fb6ba921b6da063d7668aee6c111108a8fc58d
Orlando Hoilett <orlandohoilett@pal-nat186-67-239.itap.purdue.edu>
2019-01-25 20:08:37 -0500

    really close to be exact with the VSP

--- examples/SquareWaveVoltammetry-wDueDAC/SquareWaveVoltammetry-wDueDAC.ino
+++ examples/SquareWaveVoltammetry-wDueDAC/SquareWaveVoltammetry-wDueDAC.ino
@@ -4,21 +4,19 @@
 
 LMP91000 pStat = LMP91000();
 
-int settling_time = 1;
-
-uint16_t opVolt = 3280;
 
 uint16_t i = 0;
-
-double bias[] = {0, 0.01, 0.02, 0.04, 0.06, 0.08, 0.10, 0.12, 0.14, 0.16, 0.18, 0.20, 0.22, 0.24};
-uint16_t  dacVal = 1500; //in milliVolts
-uint8_t  bias_setting = 0;
+uint16_t opVolt = 3310;
 const double v_tolerance = 0.1; //10%
 
 
-uint16_t dacMin = 550; //0.55V
-uint16_t dacMax = 2750; //2.75V
-uint16_t dacSpan = 2200; //2.2V
+const uint16_t dacMin = 550; //0.55V
+const uint16_t dacMax = 2750; //2.75V
+const uint16_t dacSpan = 2200; //2.2V
+const uint16_t dacResolution = 4095; //12-bit
+
+
+const bool debug = true;
 
 
 void setup()
@@ -35,58 +33,38 @@ void setup()
   delay(50);
   pStat.standby();
   delay(50);
-  pStat.disableFET();
-  pStat.setGain(3);
-  pStat.setRLoad(0);
-  pStat.setExtRefSource();
-  pStat.setIntZ(1);
-  pStat.setThreeLead();
+  initLMP(3);  
   delay(2000); //warm-up time for the gas sensor
 
 
-  Serial.println("Time(ms),DAC Val,Bias Setting,Voltage,LMPOut,C1,C2,DACOutMeasured,Time(ms),DAC Val,Bias Setting,Voltage,LMPOut,C1,C2,DACOutMeasured,ADCs 10-bit 3.3V");
-  
+  if(debug) Serial.println("Time(ms),Voltage,VF,Sign,Bias Index,Bias,dacOut,DueDACValue,Delay,DAC Read,LMP,C1,C2,Time(ms),Voltage,VR,Sign,Bias Index,Bias,dacOut,DueDACValue,Delay,DAC Read,LMP,C1,C2");
+  else Serial.println("LMP,C1,C2,LMP,C1,C2");
+
 
   //will hold the code here until a character is sent over the serial port
   //this ensures the experiment will only run when initiated
   while(!Serial.available());
   Serial.read();
 
+
   //lmpGain, startV, endV, pulse amplitude, stepV, frequency
-  runSWV(3, 20, 500, 20, 2, 50);
+  runSWV(3, 0, 500, 20, 4, 60);
+  delay(3000);
+  
+  Serial.println("Run backwards");
+  if(debug) Serial.println("Time(ms),Voltage,VF,Sign,Bias Index,Bias,dacOut,DueDACValue,Delay,DAC Read,LMP,C1,C2,Time(ms),Voltage,VR,Sign,Bias Index,Bias,dacOut,DueDACValue,Delay,DAC Read,LMP,C1,C2");
+  else Serial.println("LMP,C1,C2,LMP,C1,C2");
+  runSWVReverse(3, 500, 0, 20, 4, 60);
 }
+
 
 void loop()
 {
-//  uint16_t randNumber = random(dacMin,dacMax);
-//
-//  i = (randNumber-dacMin)*((double)4095/dacSpan);
-//
-//  analogWrite(DAC0,i);
-//  Serial.print((double)randNumber/1000,2);
-//  Serial.print(",");
-//  Serial.println(i);
-//  
-//  
-//  delay(2000);
 }
 
 
-//Convert the desired voltage 
-void convertToArduinoDueDACVal()
+void initLMP(uint8_t lmpGain)
 {
-  dacVal = (dacVal-dacMin)*((double)4095/dacSpan);
-
-//  Serial.print("dacVal: ");
-//  Serial.print(dacVal);
-//  Serial.print(", ");
-}
-
-
-void runSWV(uint8_t lmpGain, int16_t startV, int16_t endV,
-            uint8_t pulseAmp, int16_t stepV, uint16_t freq)
-{
-  //initializes LMP91000
   pStat.disableFET();
   pStat.setGain(lmpGain);
   pStat.setRLoad(0);
@@ -95,129 +73,253 @@ void runSWV(uint8_t lmpGain, int16_t startV, int16_t endV,
   pStat.setThreeLead();
   pStat.setBias(0);
   pStat.setPosBias();
-  
+}
 
-  pStat.setPosBias();
+
+void runSWV(uint8_t lmpGain, int16_t startV, int16_t endV,
+            uint8_t pulseAmp, int16_t stepV, uint8_t freq)
+{
+  initLMP(lmpGain);
+
   for (int16_t j = startV; j <= endV; j += stepV)   //Steps from the starting voltage to the end voltage increased by the volt_step
   {
-//    Serial.print("j: ");
-//    Serial.print(j);
-//    Serial.print(", ");
-//    
-//    Serial.print("Pulse Up: ");
+    //POSITIVE pulse of square wave
+    if(debug)
+    {
+      Serial.print(millis());
+      Serial.print(",");
+      Serial.print(j);
+      Serial.print(",");
+      Serial.print(j + pulseAmp);
+      Serial.print(",");
+    }
+
+    setLMPBias(j + pulseAmp);
     setVoltage(j + pulseAmp);
-//    Serial.print(", ");
+    delay((uint8_t)(1000.0 / (2*freq)));
 
-//    Serial.print("j + pulseAmp: ");
-//    Serial.print(j+pulseAmp);
+    if(debug)
+    {
+      Serial.print((uint16_t)(1000.0 / (2*freq)));
+      Serial.print(",");
+      Serial.print(analogRead(A3)); //DAC output voltage
+      Serial.print(",");
+    }
+
+    Serial.print(analogRead(A0)); //lmp output
+    Serial.print(",");
+    Serial.print(analogRead(A1)); //C1
+    Serial.print(",");
+    Serial.print(analogRead(A2)); //C2
+    Serial.print(",");
+
+//    Serial.print(pStat.getCurrent(analogRead(A0), opVolt, 12)); //lmp output
 //    Serial.print(",");
-    
-    
-    convertToArduinoDueDACVal();
-    analogWrite(DAC0,dacVal);
-    if(j+pulseAmp < 0) pStat.setNegBias();
-    else if (j+pulseAmp > 0) pStat.setPosBias();
-    else {}
-    pStat.setBias(bias_setting);
-    delay(1000 / freq);
-    
-
-    Serial.print(millis());
-    Serial.print(",");
-    Serial.print(dacVal);
-    Serial.print(",");
-    Serial.print(bias[bias_setting]);
-    Serial.print(",");
-    Serial.print(j + pulseAmp);
-    Serial.print(",");
-    Serial.print(analogRead(A0));
-    Serial.print(",");
-    Serial.print(analogRead(A1));
-    Serial.print(",");
-    Serial.print(analogRead(A2));
-    Serial.print(",");
-    Serial.print(analogRead(A3)); //voltage output of the DAC
-    Serial.print(",");
+//    Serial.print(pStat.getCurrent(analogRead(A1), opVolt, 12)); //C1
+//    Serial.print(",");
+//    Serial.print(pStat.getCurrent(analogRead(A2), opVolt, 12)); //C2
+//    Serial.print(",");
     //delay(3000);
 
 
-    //Serial.print("Pulse Down: ");
+
+    ////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////
+    //NEGATIVE pulse of square wave
+    if(debug)
+    {
+      Serial.print(millis());
+      Serial.print(",");
+      Serial.print(j);
+      Serial.print(",");
+      Serial.print(j - pulseAmp);
+      Serial.print(",");
+    }
+
+    setLMPBias(j - pulseAmp);
     setVoltage(j - pulseAmp);
-    //Serial.println();
+    delay((uint8_t)(1000.0 / (2*freq)));
 
-//    Serial.print("j - pulseAmp: ");
-//    Serial.println(j-pulseAmp);
+    if(debug)
+    {
+      Serial.print((uint16_t)(1000.0 / (2*freq)));
+      Serial.print(",");
+      Serial.print(analogRead(A3)); //DAC output voltage
+      Serial.print(",");
+    }
 
-    
-    convertToArduinoDueDACVal();
-    analogWrite(DAC0,dacVal);
-    if(j-pulseAmp < 0) pStat.setNegBias();
-    else if (j-pulseAmp > 0) pStat.setPosBias();
-    else {}
-    pStat.setBias(bias_setting);
-    delay(1000 / freq);
-    
-    
-    Serial.print(millis());
+    Serial.print(analogRead(A0)); //lmp output
     Serial.print(",");
-    Serial.print(dacVal);
+    Serial.print(analogRead(A1)); //C1
     Serial.print(",");
-    Serial.print(bias[bias_setting]);
-    Serial.print(",");
-    Serial.print(j - pulseAmp);
-    Serial.print(",");
-    Serial.print(analogRead(A0));
-    Serial.print(",");
-    Serial.print(analogRead(A1));
-    Serial.print(",");
-    Serial.print(analogRead(A2));
-    Serial.print(",");
-    Serial.print(analogRead(A3)); //voltage output of the DAC
+    Serial.print(analogRead(A2)); //C2
     Serial.println();
+
+//    Serial.print(pStat.getCurrent(analogRead(A0), opVolt, 12)); //lmp output
+//    Serial.print(",");
+//    Serial.print(pStat.getCurrent(analogRead(A1), opVolt, 12)); //C1
+//    Serial.print(",");
+//    Serial.print(pStat.getCurrent(analogRead(A2), opVolt, 12)); //C2
+//    Serial.println();
+    //delay(3000);
+  }
+  
+}
+
+
+void runSWVReverse(uint8_t lmpGain, int16_t startV, int16_t endV,
+            uint8_t pulseAmp, int16_t stepV, uint8_t freq)
+{
+  initLMP(lmpGain);
+
+  for (int16_t j = startV; j >= endV; j -= stepV)   //Steps from the starting voltage to the end voltage increased by the volt_step
+  {
+    //POSITIVE pulse of square wave
+    if(debug)
+    {
+      Serial.print(millis());
+      Serial.print(",");
+      Serial.print(j);
+      Serial.print(",");
+      Serial.print(j + pulseAmp);
+      Serial.print(",");
+    }
+
+    setLMPBias(j + pulseAmp);
+    setVoltage(j + pulseAmp);
+    delay((uint8_t)(1000.0 / (2*freq)));
+
+    if(debug)
+    {
+      Serial.print((uint16_t)(1000.0 / (2*freq)));
+      Serial.print(",");
+      Serial.print(analogRead(A3)); //DAC output voltage
+      Serial.print(",");
+    }
+
+    Serial.print(analogRead(A0)); //lmp output
+    Serial.print(",");
+    Serial.print(analogRead(A1)); //C1
+    Serial.print(",");
+    Serial.print(analogRead(A2)); //C2
+    Serial.print(",");
+
+//    Serial.print(pStat.getCurrent(analogRead(A0), opVolt, 12)); //lmp output
+//    Serial.print(",");
+//    Serial.print(pStat.getCurrent(analogRead(A1), opVolt, 12)); //C1
+//    Serial.print(",");
+//    Serial.print(pStat.getCurrent(analogRead(A2), opVolt, 12)); //C2
+//    Serial.print(",");
     //delay(3000);
 
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////
+    //NEGATIVE pulse of square wave
+    if(debug)
+    {
+      Serial.print(millis());
+      Serial.print(",");
+      Serial.print(j);
+      Serial.print(",");
+      Serial.print(j - pulseAmp);
+      Serial.print(",");
+    }
+
+    setLMPBias(j - pulseAmp);
+    setVoltage(j - pulseAmp);
+    delay((uint8_t)(1000.0 / (2*freq)));
+
+    if(debug)
+    {
+      Serial.print((uint16_t)(1000.0 / (2*freq)));
+      Serial.print(",");
+      Serial.print(analogRead(A3)); //DAC output voltage
+      Serial.print(",");
+    }
+
+    Serial.print(analogRead(A0)); //lmp output
+    Serial.print(",");
+    Serial.print(analogRead(A1)); //C1
+    Serial.print(",");
+    Serial.print(analogRead(A2)); //C2
+    Serial.println();
+
+//    Serial.print(pStat.getCurrent(analogRead(A0), opVolt, 12)); //lmp output
+//    Serial.print(",");
+//    Serial.print(pStat.getCurrent(analogRead(A1), opVolt, 12)); //C1
+//    Serial.print(",");
+//    Serial.print(pStat.getCurrent(analogRead(A2), opVolt, 12)); //C2
+//    Serial.println();
+    //delay(3000);
+  }
+  
+}
+
+
+void setVoltage(int16_t voltage)
+{
+  uint16_t dacVout = 1500;
+  uint8_t bias_setting = 0;
+
+  if(abs(voltage) < 15) voltage = 15*(voltage/abs(voltage));    
+  
+  int16_t setV = dacVout*TIA_BIAS[bias_setting];
+  voltage = abs(voltage);
+  
+  //outside the 10% tolerance range
+  while(setV > voltage*(1+v_tolerance) || setV < voltage*(1-v_tolerance))
+  {
+    if(bias_setting == 0) bias_setting = 1;
+    
+    dacVout = voltage/TIA_BIAS[bias_setting];
+    
+    if (dacVout > dacMax)
+    {
+      bias_setting++;
+      dacVout = 1500;
+    }
+    
+    setV = dacVout*TIA_BIAS[bias_setting];    
+  }
+
+
+  pStat.setBias(bias_setting);
+  analogWrite(DAC0, convertDACVoutToArduinoDueDACVal(dacVout));
+
+  if(debug)
+  {
+    Serial.print(bias_setting);
+    Serial.print(",");
+    Serial.print(TIA_BIAS[bias_setting]);
+    Serial.print(",");
+    Serial.print(dacVout);
+    Serial.print(",");
+    Serial.print(convertDACVoutToArduinoDueDACVal(dacVout));
+    Serial.print(",");
   }
 }
 
 
-int16_t setVoltage(int16_t num)
+//Convert the desired voltage
+uint16_t convertDACVoutToArduinoDueDACVal(uint16_t dacVout)
 {
-//  Serial.print("num: ");
-//  Serial.print(num);
+  return (dacVout-dacMin)*((double)dacResolution/dacSpan);
+}
 
+
+void setLMPBias(int16_t voltage)
+{
+  signed char sign = (double)voltage/abs(voltage);
   
-  if(abs(num) < 15) num = 15*(num/abs(num));
-  
-  dacVal = 1500;
-  bias_setting = 0;
-  
-  
-  int16_t setV = dacVal*bias[bias_setting];
-  num = abs(num);
-  
-  //outside the 10% tolerance range
-  while(setV > num*(1+v_tolerance) || setV < num*(1-v_tolerance))
+  if(sign < 0) pStat.setNegBias();
+  else if (sign > 0) pStat.setPosBias();
+  else {} //do nothing
+
+  if(debug)
   {
-    if(bias_setting == 0) bias_setting = 1;
-    
-    dacVal = num/bias[bias_setting];
-    
-    //if (dacVal > 2950)
-    if (dacVal > dacMax)
-    {
-      bias_setting++;
-      dacVal = 1500;
-    }
-    
-    setV = dacVal*bias[bias_setting];    
+    Serial.print(sign);
+    Serial.print(",");
   }
-
-//  Serial.print(", ");
-//  Serial.print("dacVal: ");
-//  Serial.print(dacVal);
-//  Serial.print(", ");
-//  Serial.print("bias_setting: ");
-//  Serial.print(bias[bias_setting]);
-  
-  return setV;
 }
